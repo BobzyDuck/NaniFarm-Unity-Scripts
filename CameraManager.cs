7263a71cedc3e7ec2dab71d8adab49d93ba39a1d
Bobzy Duck <atrisapic@gmail.com>
2026-06-08 15:53:01 +0330

    Add files via upload

---
+++ CameraManager.cs
@@ -0,0 +1,123 @@
+﻿using Unity.Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance { get; private set; }
+
+    [SerializeField] public Camera _mainCamera;
+    [SerializeField] public CinemachineCamera _firstCamera; 
+
+    private float _originalFOV;
+    private float _enteredFOV; 
+    private Coroutine _fovCoroutine;
+    private Coroutine _rotationCoroutine;
+
+    private float _originalRotation;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void Start()
+    {
+        _originalFOV = _firstCamera.Lens.FieldOfView; 
+        _originalRotation = _firstCamera.transform.rotation.eulerAngles.y; 
+    }
+
+    public float GetOriginalRotation()
+    {
+        return _originalRotation;
+    }
+
+    public float GetOriginalFOV()
+    {
+        return _originalFOV;
+    }
+
+    public void ChangeFOV(float newFOV, bool isEntering, float duration)
+    {
+        if (_fovCoroutine != null)
+        {
+            StopCoroutine(_fovCoroutine);
+        }
+
+        if (isEntering)
+        {
+            _enteredFOV = _firstCamera.Lens.FieldOfView;
+        }
+
+        _fovCoroutine = StartCoroutine(ChangeFOVCoroutine(newFOV, isEntering, duration));
+    }
+
+    private IEnumerator ChangeFOVCoroutine(float newFOV, bool isEntering, float duration)
+    {
+        float elapsedTime = 0f;
+        float startingFOV = isEntering ? _enteredFOV : _firstCamera.Lens.FieldOfView; 
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+
+            t = Mathf.SmoothStep(0f, 1f, t);
+
+            _firstCamera.Lens.FieldOfView = Mathf.Lerp(startingFOV, newFOV, t); 
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _firstCamera.Lens.FieldOfView = newFOV; 
+        _fovCoroutine = null;
+    }
+
+    public void ChangeCameraRotation(float targetRotation, float duration)
+    {
+        if (_rotationCoroutine != null)
+        {
+            StopCoroutine(_rotationCoroutine);
+        }
+
+        _rotationCoroutine = StartCoroutine(ChangeRotationCoroutine(targetRotation, duration));
+    }
+
+    private IEnumerator ChangeRotationCoroutine(float targetRotation, float duration)
+    {
+        float elapsedTime = 0f;
+        float startingRotation = _firstCamera.transform.rotation.eulerAngles.y;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+
+            t = Mathf.SmoothStep(0f, 1f, t);
+
+            float currentRotation = Mathf.LerpAngle(startingRotation, targetRotation, t);
+            Vector3 currentEulerAngles = _firstCamera.transform.rotation.eulerAngles;
+            _firstCamera.transform.rotation = Quaternion.Euler(currentEulerAngles.x, currentRotation, currentEulerAngles.z);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Vector3 finalEulerAngles = _firstCamera.transform.rotation.eulerAngles;
+        _firstCamera.transform.rotation = Quaternion.Euler(finalEulerAngles.x, targetRotation, finalEulerAngles.z);
+
+        _rotationCoroutine = null;
+    }
+
+    public void SetCameraInstantly(float newFOV, float newRotation)
+    {
+        _firstCamera.Lens.FieldOfView = newFOV;  
+        Vector3 currentEulerAngles = _firstCamera.transform.rotation.eulerAngles;
+        _firstCamera.transform.rotation = Quaternion.Euler(currentEulerAngles.x, newRotation, currentEulerAngles.z);
+    }
+}
---
+++ CameraTransitionZone.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransitionZone : MonoBehaviour
+{
+    private CameraManager cameraManager;
+
+    [Header("FOV Duration Settings")]
+    [SerializeField] private float fovDurationEnter = 2.0f; 
+    [SerializeField] private float fovDurationExit = 4.0f; 
+
+    [Header("Rotation Settings")]
+    [SerializeField] private bool enableRotation = false;
+    [SerializeField] private float targetRotationEnter = 45.0f;
+
+    [SerializeField] private float _targetFOV;
+
+    private void Start()
+    {
+        cameraManager = CameraManager.Instance;
+        if (cameraManager == null)
+        {
+            Debug.LogError("CameraManager not found in the scene.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && cameraManager != null)
+        {
+            cameraManager.ChangeFOV(_targetFOV, true, fovDurationEnter); 
+
+            if (enableRotation)
+            {
+                cameraManager.ChangeCameraRotation(targetRotationEnter, fovDurationEnter); 
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && cameraManager != null)
+        {
+            cameraManager.ChangeFOV(cameraManager.GetOriginalFOV(), false, fovDurationExit);
+
+            if (enableRotation)
+            {
+                cameraManager.ChangeCameraRotation(cameraManager.GetOriginalRotation(), fovDurationExit);
+            }
+        }
+    }
+}
